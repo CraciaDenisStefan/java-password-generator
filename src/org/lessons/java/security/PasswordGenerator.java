0e914e44d0c0a16d905547f5d652365a85df22d7
CraciDenisStefan <crdenis13@gmail.com>
2023-11-09 16:52:29 +0100

    finito

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,46 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		
+		   Scanner in = new Scanner(System.in);
+
+	     
+	        System.out.print("Inserisci il tuo nome:");
+	        String nome = in.nextLine();
+	        
+	     
+	        System.out.print("Inserisci il tuo cognome:");
+	        String cognome = in.nextLine();
+
+	  
+	        System.out.print("Inserisci il tuo colore preferito:");
+	        String colorePreferito = in.nextLine();
+
+	
+	        System.out.print("Inserisci il giorno di nascita:");
+	        String giornoNascita = in.nextLine();
+	        int giorno = Integer.valueOf(giornoNascita);
+	        
+	        System.out.print("Inserisci il mese di nascita:");
+	        String meseNascita = in.nextLine();
+	        int mese = Integer.valueOf(meseNascita);
+
+	        System.out.print("Inserisci l'anno di nascita:");
+	        String annoNascita = in.nextLine();
+	        int anno = Integer.valueOf(annoNascita);
+
+	     
+	        int sommaNascita = giorno + mese + anno;
+
+	  
+
+	     
+	        System.out.println("La tua password generata è:"+ nome + "-" + cognome + "-" + colorePreferito + "-" + sommaNascita);
+
+	   
+	        in.close();
 	}
 }
